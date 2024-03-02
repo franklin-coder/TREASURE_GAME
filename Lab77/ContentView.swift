f15922b99773ce338ffd420b34cd2311e564f18c
franklin-coder <franklin.tejadag@gmail.com>
2024-03-01 16:47:21 -0800

    Initial Commit

---
+++ Lab77/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab77
+//
+//  Created by IMD 224 on 2024-03-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab77/Lab77App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab77App.swift
+//  Lab77
+//
+//  Created by IMD 224 on 2024-03-01.
+//
+
+import SwiftUI
+
+@main
+struct Lab77App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
